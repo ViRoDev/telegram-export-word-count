d00380800c34354f9acfb82b139d64c4c48ea5fd
Vitaly Romanov <102828415+ViRoDev@users.noreply.github.com>
2022-09-03 20:51:23 +0300

    added additional functions for message array manipulation

--- src/filterMessages.ts
+++ src/filterMessages.ts
@@ -1,13 +1,21 @@
-import { CombinedMessage, CombinedProperties, Message, MessageTypes } from "messages.types";
+import { CombinedProperties, Messages, MessageTypes } from "messages.types";
 
 const filteredMessages = (type: MessageTypes, 
                           property: CombinedProperties, 
                           value: string,
-                          messages: Array<Message>
+                          messages: Messages
                          ) => 
     messages.filter(m => 
         m.type === type && m[property] === value
     )
 
+export const propertyOnly = (property: CombinedProperties, messages: Messages) => 
+        messages.map(m => m[property]);
+
+export const textOnly = (messages: Messages) => 
+    propertyOnly("text", messages);
+
+export const textOnlyNonEmpty = (messages: Messages) => 
+        textOnly(messages).filter(Boolean);
 
 export default filteredMessages;
