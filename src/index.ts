e605b80bfcd7a3cd34ca4adfc8aa56f43e1e07f3
Vitaly Romanov <102828415+ViRoDev@users.noreply.github.com>
2022-09-03 19:49:40 +0300

    refactored code into smaller functions, added saving of transitional result into json for optimization

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,24 @@
+import fs from 'fs';
+import {config} from 'dotenv'
 import filteredMessages from "filterMessages";
 import parseJSON from "./parseJSON";
+import { Message } from 'messages.types';
 
 const FILEPATH = 'public/result.json';
+config({path: './public/.env'});
 
 const main = async () => {
-    const dataObject = await parseJSON(FILEPATH);
+    const messages = await getMessagesFromFile(FILEPATH)
+    const filtered = filterFromUser(messages)
+    const textOnly = filtered.map(msg => msg.text)
+    //or open already existing file 
+    fs.writeFileSync("public/filtered.json", JSON.stringify(textOnly));
 }
+
+const getMessagesFromFile = async (filepath: string) => 
+    (await parseJSON(filepath)).messages
+
+const filterFromUser = (messages : Array<Message>) => 
+    filteredMessages('message', 'from_id', process.env.FROM_ID ?? "user01", messages)
 
 main();
