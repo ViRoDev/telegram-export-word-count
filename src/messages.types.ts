e5c48456a6a68ebfe170430ab6627e4da11a1da6
Vitaly Romanov <102828415+ViRoDev@users.noreply.github.com>
2022-09-03 18:44:46 +0300

    added new messages types

--- src/messages.types.ts
+++ src/messages.types.ts
@@ -1,6 +1,7 @@
 export type MessageTypes = "service" | "message" 
 export type ActionTypes = "edit_group_photo" | "migrate_from_group" | "invite_members"
 
+export type MessageProperties = "id" | "type" | "date" | "date_unixtime" | "text";
 export type Message = {
     id: number,
     type: MessageTypes,
@@ -9,14 +10,19 @@ export type Message = {
     text: string,
 }
 
+export type ServiceMessageProperties = MessageProperties | "actor" | "actor_id" | "action";
 export type ServiceMessage = Message & {
     actor: string,
     actor_id: number,
     action: ActionTypes,
 }
 
+export type UserMessageProperties = MessageProperties | "from" | "from_id" | "reply_to_message_id";
 export type UserMessage = Message & {
     from: string,
     from_id: number,
     reply_to_message_id?: number,
 }
+
+export type CombinedProperties = UserMessageProperties | ServiceMessageProperties
+export type CombinedMessage = UserMessage & ServiceMessage;
