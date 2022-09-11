fa32b22a3b8471857813c740e922f6a3e718a37e
Vitaly Romanov <102828415+ViRoDev@users.noreply.github.com>
2022-09-12 02:08:56 +0300

    Fixed? cache empty array problem

--- src/functionMemoization.ts
+++ src/functionMemoization.ts
@@ -7,42 +7,34 @@ export const sha256Hex = (...args : any) : hex =>
     createHash('sha256').update(args.toString()).digest('hex');
 
 export const cache = <T>(func : Function, cacheFolderPath : string = '.cache') => {
-    type cacheObjectType = { [key: string] : any}
-    // let mem : cacheObjectType;
-    // try {
-    //     mem = JSON.parse(fs.readFileSync(cacheFolderPath).toString()) as cacheObjectType;
-    // }
-    // catch(err) {
-    //     console.log('Cache file not found... Creating empty cache');
-    //     mem = {}
-    // }
-    
-    const memFunc = async (...args : any) => {
-        const functionCacheHex = sha256Hex(func.toString(), args)
-        console.log(func.toString())
-        let mem : cacheObjectType;
-        const functionCachePath = `${cacheFolderPath}/${functionCacheHex}`;
+    type cacheObjectType = { [hash: string] : any}   
+    const memFunc = (...args : any) => {
+        const cacheHex = sha256Hex(func.toString(), args.toString())
+        const functionCachePath = `${cacheFolderPath}/${cacheHex}`;
+        let mem : cacheObjectType = {};
+
         try { 
             mem = JSON.parse(
             fs.readFileSync(functionCachePath)
             .toString()) as cacheObjectType;
         }
         catch(err) {
-            mem = {};
+            console.log(`No cached files found. New dictionary will be created...`)
         }
         
-        if(!mem[functionCacheHex]) {
+        if(!mem[cacheHex]) {
             //TODO: make encryption/decryption of function call result
-            mem[functionCacheHex] = await func(...args);
+            const funcResults = func(...args);
+            mem[cacheHex] = funcResults;
 
             try { fs.writeFileSync(functionCachePath, JSON.stringify(mem)) }
             catch (err) { 
                 console.log(
-                    `Could not create cache file at ${cacheFolderPath}. \n
+                    `Could not create cache file at ${functionCachePath}. \n
                     Complete error: ${err}`)
             }
         }
-        return mem[functionCacheHex]
+        return mem[cacheHex]
     }
     return memFunc as T;
 }
